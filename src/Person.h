9b7d914029ecb230b0afd0698f492b328d631fdd
mobina <68993058+mobinaghrz@users.noreply.github.com>
2026-01-08 23:55:39 +0000

    Creating the Person header and the class

---
+++ src/Person.h
@@ -0,0 +1,10 @@
+// If PERSON_H is not defined, define it (start of header guard)
+#ifndef PERSON_H
+// Define PERSON_H to prevent multiple inclusions
+#define PERSON_H
+
+class Person{
+
+};
+
+#endif
