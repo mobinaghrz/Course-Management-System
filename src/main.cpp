d8da84a43192483f27fc60543c3556d7b1861780
mobina <68993058+mobinaghrz@users.noreply.github.com>
2026-01-10 05:30:00 +0000

    main lists

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+
+int main(){
+    //Choosing the Action
+    list<string> Actions = {
+                                "Add Student",
+                                "Add Course",
+                                "Add Instructor",
+                                "Enroll Student in Course",
+                                "Drop Course",
+                                "Add Grade",
+                                "View Student Details",
+                                "View Course Details",
+                                "List All Students",
+                                "List All Courses",
+                                "Search Students by Name",
+                                "Calculate Student GPA",
+                                "Save Data to Files",
+                                "Load Data from Files",
+                                "Exit Program"
+                            };
+
+    for (string i : Actions){
+        cout<< i<<endl;
+    };
+    while(true){
+
+    };
+    return 0;
+};
