41a493ec6bb88021e4bdafd4030364c563b37bd8
mobina <68993058+mobinaghrz@users.noreply.github.com>
2026-01-17 02:23:58 +0000

    Person Cpp update

--- src/Person.cpp
+++ src/Person.cpp
@@ -1,7 +1,12 @@
 #include <string>
+#include <iostream>
 #include "Person.h"
 
-Person::Person(const int& id, const std::string& name, std::string& email):id(id),name(name),email(email),age(0){
+Person::Person(const int& id, const std::string& name, std::string& email):id(id),name(name),email(email),age(0){};
 
-
+void Person::Save(std::ostream& out) const{
+    out << id << "\n";
+    out << name << "\n";
+    out << age << "\n";
+    out << email << "\n";
 }
