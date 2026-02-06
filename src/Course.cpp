686a587b136bd0cb323e9ccde384f1b755f25abd
Mobina <68993058+mobinaghrz@users.noreply.github.com>
2026-02-06 03:41:55 +0000

    Course.h define

--- src/Course.cpp
+++ src/Course.cpp
@@ -1,3 +1,16 @@
 #include "Course.h"
+#include<iostream>
+#include<vector>
 
-Course::Course(const std::string& courseName, int courseCredits): name(courseName), credits(courseCredits) {}
+Course::Course(const std::string& courseName, int courseCredits): name(courseName), credits(courseCredits) {};
+
+bool Course::addStudent(int StudentID){
+    if(std::find(Enrolled_Students.begin(), Enrolled_Students.end(), StudentID) != Enrolled_Students.end()){
+        return 0;
+    }
+
+    else {
+        Enrolled_Students.insert(Enrolled_Students.end(),StudentID);
+        return 1;
+    };
+};
